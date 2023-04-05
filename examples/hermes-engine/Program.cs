1f7e41b49aba8ae555f173277c2e66c8ad39664b
Vladimir Morozov <vmorozov@microsoft.com>
2023-04-04 19:18:11 -0700

    Simplify usage

--- examples/hermes-engine/Program.cs
+++ examples/hermes-engine/Program.cs
@@ -1,27 +1,13 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Runtime.InteropServices;
 using Hermes.Example;
 using Microsoft.JavaScript.NodeApi;
-using static Hermes.Example.HermesInterop;
 using static Microsoft.JavaScript.NodeApi.JSNativeApi.Interop;
 
-nint hermesLib = NativeLibrary.Load("hermes.dll");
-HermesInterop.Initialize(hermesLib);
-JSNativeApi.Interop.Initialize(hermesLib);
+HermesApi.Load("hermes.dll");
+using var runtime = new HermesRuntime();
+using var scope = new JSValueScope(JSValueScopeType.Root, (napi_env)runtime);
 
-hermes_create_config(out hermes_config config);
-hermes_create_runtime(config, out hermes_runtime runtime);
-hermes_get_node_api_env(runtime, out napi_env env);
-
-{
-    using var scope = new JSValueScope(JSValueScopeType.Root, env);
-    JSNativeApi.RunScript("x = 2");
-    Console.WriteLine($"Result: {(int)JSValue.Global["x"]}");
-}
-
-Console.WriteLine("Hello, World!");
-
-hermes_delete_runtime(runtime);
-hermes_delete_config(config);
+JSNativeApi.RunScript("x = 2");
+Console.WriteLine($"Result: {(int)JSValue.Global["x"]}");
