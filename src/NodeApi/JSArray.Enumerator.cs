0f8d09809b4acec54d307447077eec216dfaa3dc
Vladimir Morozov <vmorozov@microsoft.com>
2023-12-10 17:42:32 -0800

    Update JSValue

--- src/NodeApi/JSArray.Enumerator.cs
+++ src/NodeApi/JSArray.Enumerator.cs
@@ -1,20 +1,15 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
-
 namespace Microsoft.JavaScript.NodeApi;
 
 public partial struct JSArray
 {
-    public struct Enumerator : IEnumerator<JSValue>, IEnumerator
+    public ref struct Enumerator
     {
         private readonly JSValue _array;
         private readonly int _count;
         private int _index;
-        private JSValue? _current;
 
         internal Enumerator(JSValue array)
         {
@@ -27,48 +22,20 @@ public partial struct JSArray
             {
                 _count = 0;
             }
-            _index = 0;
-            _current = default;
+            _index = -1;
         }
 
         public bool MoveNext()
         {
-            if (_index < _count)
+            if (_index + 1 < _count)
             {
-                _current = _array.GetElement(_index);
                 _index++;
                 return true;
             }
 
-            _index = _count + 1;
-            _current = default;
             return false;
         }
 
-        public readonly JSValue Current
-            => _current ?? throw new InvalidOperationException("Unexpected enumerator state");
-
-        readonly object? IEnumerator.Current
-        {
-            get
-            {
-                if (_index == 0 || _index == _count + 1)
-                {
-                    throw new InvalidOperationException("Invalid enumerator state");
-                }
-                return Current;
-            }
-        }
-
-        void IEnumerator.Reset()
-        {
-            _index = 0;
-            _current = default;
-        }
-
-        readonly void IDisposable.Dispose()
-        {
-        }
+        public readonly JSValue Current => _array[_index];
     }
 }
-
--- src/NodeApi/JSArray.cs
+++ src/NodeApi/JSArray.cs
@@ -8,7 +8,7 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Microsoft.JavaScript.NodeApi;
 
-public readonly partial struct JSArray : IList<JSValue>, IEquatable<JSValue>
+public readonly ref partial struct JSArray
 {
     private readonly JSValue _value;
 
@@ -34,23 +34,7 @@ public readonly partial struct JSArray : IList<JSValue>, IEquatable<JSValue>
     {
     }
 
-    public JSArray(JSValue[] array)
-    {
-        // Do not specify the length because it will create a sparse array
-        // that prevents some V8 optimizations.
-        _value = JSValue.CreateArray();
-        for (int i = 0; i < array.Length; i++)
-        {
-            _value.SetElement(i, array[i]);
-        }
-    }
-
-    /// <inheritdoc/>
     public int Length => _value.GetArrayLength();
-
-    int ICollection<JSValue>.Count => _value.GetArrayLength();
-
-    bool ICollection<JSValue>.IsReadOnly => false;
 
     public JSValue this[int index]
     {
@@ -58,74 +42,20 @@ public readonly partial struct JSArray : IList<JSValue>, IEquatable<JSValue>
         set => _value.SetElement(index, value);
     }
 
-    /// <inheritdoc/>
     public void Add(JSValue item) => _value["push"].Call(_value, item);
 
-    /// <inheritdoc/>
-    public void CopyTo(JSValue[] array, int arrayIndex)
-    {
-        int i = arrayIndex;
-        foreach (JSValue item in this)
-        {
-            array[i++] = item;
-        }
-    }
-
-    /// <summary>
-    /// Copies array elements to a destination array, converting each element from JS
-    /// values using a conversion delegate.
-    /// </summary>
-    /// <param name="array">Destination array.</param>
-    /// <param name="arrayIndex">Starting index in the destination array.</param>
-    /// <param name="fromJS">Delegate that converts from JS value to array element type.</param>
-    public void CopyTo<T>(T[] array, int arrayIndex, JSValue.To<T> fromJS)
-    {
-        int i = arrayIndex;
-        foreach (JSValue item in this)
-        {
-            array[i++] = fromJS(item);
-        }
-    }
-
-    /// <summary>
-    /// Copies array elements from a source array, converting each element to JS values
-    /// using a conversion delegate.
-    /// </summary>
-    /// <param name="array">Source array.</param>
-    /// <param name="arrayIndex">Starting index in the destination array.</param>
-    /// <param name="toJS">Delegate that converts from array element type to JS value.</param>
-    public void CopyFrom<T>(T[] array, int arrayIndex, JSValue.From<T> toJS)
-    {
-        int i = arrayIndex;
-        foreach (T item in array)
-        {
-            this[i++] = toJS(item);
-        }
-    }
-
-    /// <inheritdoc/>
     public Enumerator GetEnumerator() => new(_value);
 
-    IEnumerator<JSValue> IEnumerable<JSValue>.GetEnumerator() => GetEnumerator();
-
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-    /// <inheritdoc/>
     public int IndexOf(JSValue item) => (int)_value.CallMethod("indexOf", item);
 
-    /// <inheritdoc/>
     public void Insert(int index, JSValue item) => _value.CallMethod("splice", index, 0, item);
 
-    /// <inheritdoc/>
     public void RemoveAt(int index) => _value.CallMethod("splice", index, 1);
 
-    /// <inheritdoc/>
     public void Clear() => _value.CallMethod("splice", 0, Length);
 
-    /// <inheritdoc/>
     public bool Contains(JSValue item) => IndexOf(item) >= 0;
 
-    /// <inheritdoc/>
     public bool Remove(JSValue item)
     {
         int index = IndexOf(item);
@@ -144,21 +74,13 @@ public readonly partial struct JSArray : IList<JSValue>, IEquatable<JSValue>
     /// </summary>
     public static bool operator !=(JSArray a, JSArray b) => !a._value.StrictEquals(b);
 
-    /// <summary>
-    /// Compares two JS values using JS "strict" equality.
-    /// </summary>
-    public bool Equals(JSValue other) => _value.StrictEquals(other);
-
     /// <inheritdoc/>
     public override bool Equals([NotNullWhen(true)] object? obj)
-    {
-        return obj is JSValue other && Equals(other);
-    }
+       => throw new NotSupportedException(
+           "Equals for JS values is not supported. Use == operator.");
 
     /// <inheritdoc/>
     public override int GetHashCode()
-    {
-        throw new NotSupportedException(
-            "Hashing JS values is not supported. Use JSSet or JSMap instead.");
-    }
+        => throw new NotSupportedException(
+           "Hashing JS values is not supported. Use JSSet or JSMap instead.");
 }
